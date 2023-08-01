18ecc75ecdd130caf5dd0d2a31337a13deea1e02
Artem Potemkin <art.potemkin@gmail.com>
2023-08-01 19:31:48 +0300

    first part

---
+++ HW 3.1 SpringAnimation/Models/Animation.swift
@@ -0,0 +1,28 @@
+//
+//  Animation.swift
+//  HW 3.1 SpringAnimation
+//
+//  Created by Артём Потёмкин on 01.08.2023.
+//
+
+import Foundation
+
+struct Animation {
+    let preset: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+    
+    static func getAnimation() -> Animation {
+        let animation = Animation(
+            preset: DataStore.shared.presets.randomElement() ?? "pop",
+            curve: DataStore.shared.curves.randomElement() ?? "easeIn",
+            force: Double.random(in: 1.0...2.3),
+            duration: Double.random(in: 0.5...3.0),
+            delay: Double.random(in: 0.0...2.0)
+        )
+        return animation
+    }
+    
+}
--- HW 3.1 SpringAnimation/ViewControllers/ViewController.swift
+++ HW 3.1 SpringAnimation/ViewControllers/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet var springAnimationView: SpringView!
     @IBOutlet var startAnimationButton: SpringButton!
     
+    let animation = Animation.getAnimation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,6 +25,12 @@ class ViewController: UIViewController {
 
 
     @IBAction func startSpringAnimation(_ sender: SpringButton) {
+        springAnimationView.animation = animation.preset
+        springAnimationView.curve = animation.curve
+        springAnimationView.force = animation.force
+        springAnimationView.duration = animation.duration
+        springAnimationView.delay = animation.delay
+        springAnimationView.animate()
       
     }
 }
