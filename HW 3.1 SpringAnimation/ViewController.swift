ce402f14f6e36e02d8075eace2efe9894a41c749
Artem Potemkin <art.potemkin@gmail.com>
2023-08-01 18:34:00 +0300

    Lesson learn

--- HW 3.1 SpringAnimation/ViewController.swift
+++ HW 3.1 SpringAnimation/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var springAnimationView: SpringView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        springAnimationView.layer.cornerRadius = 15
     }
 
 
+    @IBAction func startSpringAnimation(_ sender: SpringButton) {
+        springAnimationView.animation = "shake"
+        springAnimationView.curve = "easeOut"
+        springAnimationView.force = 2
+        springAnimationView.animate()
+        
+        sender.animation = "fadeInLeft"
+    
+        sender.animate()
+    }
 }
 
